57c0719314304bd370e43ad8e6ac9999f5803d2e
Elli Shim <ellishim@gmail.com>
2018-01-25 09:07:26 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return Object.assign({}, state, {
+        friends: [
+          ...state.friends, action.friend
+        ]})
+    case 'REMOVE_FRIEND':
+      const friend = state.friends.findIndex(friend => friend.id === action.id)
+      return Object.assign({}, state, {friends: [
+        ...state.friends.slice(0, friend), ...state.friends.slice(friend + 1)
+      ]})
+    default:
+      return state
+
+  }
 }
