2565642113c52df7b9693e8df55b0582bb9fd390
bmcilhenny <bmcilhen@gmail.com>
2018-01-25 13:28:11 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let friends = state.friends.concat({name: action.friend.name, id: action.friend.id, hometown: action.friend.hometown})
+      return {friends: friends}
+    case 'REMOVE_FRIEND':
+      let newFriends = state.friends.filter((friend) => {
+        return friend.id !== action.id;
+      });
+      return {friends: newFriends}
+    default:
+      return state;
+  }
 }
