ca8fd3fb0c582a6064424e8b70f385b2b1a817b4
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2018-01-25 12:33:31 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+switch(action.type) {
+  case 'ADD_FRIEND':
+    return {friends: [...state.friends, action.friend]}
+  case 'REMOVE_FRIEND':
+    const friends = state.friends.filter(friend => friend.id !== action.id)
+    return Object.assign({}, state, {friends: friends})
+  default:
+    return state
+
+  }
 }
